af27ba388cdbcc1ba8810bb939686d57c4f6023d
Shoriful <shoriful19@yahoo.com>
2023-04-01 23:50:50 +0600

    dilam

---
+++ first/variable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int rahimAge = 69;
+    float Month = 22.5;
+    char Chele = 'M';
+    printf("rahimer boyos %d, Month %f, Cheler Name %c", rahimAge,Month,Chele);
+    return 0;
+}
