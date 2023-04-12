26915384ded6ec9ddb8962a50776fa8a0537f628
Shoriful <shoriful19@yahoo.com>
2023-04-12 22:08:44 +0600

    array

---
+++ array/array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  int ar[10];
+
+
+  for (int i=0; i<10;i++){
+    scanf("%d",&ar[i]);
+  }
+  //scanf("%d",&ar[2]);
+
+
+  printf("%d",ar[5]);
+
+
+      return 0;
+}
