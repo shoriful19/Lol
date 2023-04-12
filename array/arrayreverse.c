254378b44ad75831b68969fe97d5e5c49e1fc43a
Shoriful <shoriful19@yahoo.com>
2023-04-12 22:31:59 +0600

    array new

---
+++ array/arrayreverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+  int n;
+  scanf("%d\n",&n);
+  int ar[n];
+  for (int i=0;i<n;i++)
+  {
+    scanf("%d",&ar[i]);
+  }
+
+for (int i=n-1;i>=0;i--)
+  {
+    printf("%d \n",ar[i]);
+  }
+
+      return 0;
+}
